873ff8d2fa378a2d7a3f993b647c7ac3592736c8
david pedraza <davlpedraza@soy.sena.edu.co>
2025-12-11 07:32:18 -0500

    feat: correcion de settings

--- PROYECTO_ARTES_ESTILOS/core/urls.py
+++ PROYECTO_ARTES_ESTILOS/core/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-
+from . import views
 
 urlpatterns = [
-path('admin/', admin.site.urls),
-path('', include('core.urls')), # URLs de core
-# path('productos/', include('productos.urls')), # Otras apps
+    path('', views.index, name='index'),  # Tu vista principal
+    # path('productos/', views.productos, name='productos'),  # Agrega más rutas aquí
 ] 
